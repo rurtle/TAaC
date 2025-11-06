9079a6a0f1ffe6f2e26e1c97c347c45628297748
Rurtle Coder <rurtle.coder@gmail.com>
2025-11-06 07:33:15 +0000

    Updated the README and also, modified some of the stubs from cradle.py

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 My attempt at building a compiler from scratch.
 
+## Resources
+
+1. [Let's Build a Compiler](https://github.com/lotabout/Let-s-build-a-compiler)
+2. [Let's Build a Compiler, by Jack Cranshaw](https://compilers.iecc.com/crenshaw/)
+
 ## Prerequisites
 
 1. Python3 version: `3.10`+
--- src/cradle.py
+++ src/cradle.py
@@ -5,61 +5,73 @@ import io
 
 ## Variable declarations
 look = ''  # Lookahead character
-input_stream = ''  # Input stream
 
 def get_char():
     """Read new character from input stream"""
-    global look, input_stream
-    pass
+    global look
+    look = sys.stdin.read(1)
 
 def report_error(s):
     """Report an error"""
-    pass
+    print(f"Error: {s}")
+
 
 def report_abort(s):
     """Report error and halt"""
-    pass
+    report_error(s)
+    sys.exit(1)
+
 
 def report_expected(s):
     """Report what was expected"""
-    pass
+    report_abort(f"{s} Expected")
+
 
 def match_char(x):
     """Match a specific input character"""
     pass
 
+
 def is_alpha_char(c):
     """Recognize an alpha character"""
     pass
+
 
 def is_digit_char(c):
     """Recognize a decimal digit"""
     pass
 
+
 def get_identifier():
     """Get an identifier"""
     pass
+
 
 def get_number():
     """Get a number"""
     pass
 
+
 def emit(s):
     """Output a string with tab"""
     pass
+
 
 def emit_ln(s):
     """Output a string with tab and CRLF"""
     pass
 
+
 def initialize():
     """Initialize"""
     pass
+
 
 def main():
     """Main program"""
     initialize()
     print("Hello, World!")
+
 
 if __name__ == "__main__":
     main()
